a0de136765b1a43675e7f5eb0d83b7de8044601b
Swapnilkumar Dwivedi <122719184+swappy-2003@users.noreply.github.com>
2024-06-11 16:51:47 +0530

    Update About.jsx

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -9,7 +9,7 @@ function About() {
           <h1 className='pt-10 text-[55px]'> Our approach:</h1>
           <button className='px-10 py-6 bg-[#212121] rounded-full text-white flex gap-10 items-center uppercase '>Read More <div className=' w-3  h-3 bg-white rounded-full '></div></button>
           </div>
-        <div className='w-1/2 h-[70vh] rounded-mdpt-3 mt-[15px]'><img src='src\assets\photo-1583156340160-7867f31285d5.avif' alt="" className='w-full h-[70vh]' /></div>
+        <div className='w-1/2 h-[70vh] rounded-mdpt-3 mt-[15px]'><img src='https://images.unsplash.com/photo-1583156340160-7867f31285d5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJhdG1hbnxlbnwwfHwwfHx8MA%3D%3D' alt="" className='w-full h-[70vh]' /></div>
         </div>
     </div>
   )
